dcd337a472ce42d67f33be4ca23b0e3c82aa9c6c
zaid <alexsanderson610@gmail.com>
2023-12-13 12:47:37 +0100

    first commit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Scout\Searchable;
 
-class Post extends Model
+class Post extends Model 
 {
     use Searchable;   
 
